cabd5d714e19cb3f7959ca5a2d0c7a6b0551b306
altescape <hello@michaelwatts.me>
2014-07-10 17:49:05 +0100

    Yay! Added a calculation

--- app/js/chart_controllers.js
+++ app/js/chart_controllers.js
@@ -79,8 +79,101 @@ angular.module('myApp.chart_controllers', [])
 
       $scope.updateChart = function () {
 
+        // Revenue Integrity
         $scope.var_revenue_integrity = Math.round( ( (0.02 * $scope.cal.param6) + (0.05 * ($scope.cal.param7 * $scope.cal.param8 / 100)) + (0.01 * $scope.cal.param6) ) / $scope.cal.adjustment );
         $scope.var_revenue_integrity_low = Math.round( ( (0.01 * $scope.cal.param6) + (0.03 * ($scope.cal.param7 * $scope.cal.param8 / 100)) + (0.001 * $scope.cal.param6) ) / $scope.cal.adjustment );
+
+        // Revenue Integrity process improvement
+        // ref:1
+        $scope.ref1 = function (val) {
+          return Math.round($scope.cal.param1 * 0.036);
+        };
+        // ref:2
+        $scope.ref2 = function (val) {
+          return $scope.ref1(val) * 1.8;
+        };
+        // ref:3
+        $scope.ref3 = function (val) {
+          return $scope.ref1(val) * 1.6
+        };
+        // ref:4
+        $scope.ref4 = function (val) {
+          return $scope.ref2(val) / $scope.ref1(val) * $scope.ref3(val) / $scope.ref1(val);
+        };
+        // ref:5
+        $scope.ref5 = function (val) {
+          return $scope.ref4(val) * $scope.ref1(val);
+        };
+        // ref:6
+        $scope.ref6 = function (val) {
+          var g30 = $scope.ref5(val) * 0.000034,
+              g31 = $scope.ref5(val) * 0.024916,
+              g32 = $scope.ref5(val) * 0.230518,
+              g33 = $scope.ref5(val) * 0.023052,
+              g34 = $scope.ref5(val) * 0.001330,
+              g35 = $scope.ref5(val) * 0.010752,
+              g36 = $scope.ref5(val) * 0.000012,
+              g37 = $scope.ref5(val) * 0.054547,
+              g38 = $scope.ref5(val) * 0.006698;
+
+          if (val === 'high') {
+            g30 = g30 / 2;
+            g31 = g31 / 2;
+            g32 = g32 / 2;
+            g33 = g33 / 2;
+            g34 = g34 / 2;
+            g35 = g35 / 2;
+            g36 = g36 / 2;
+            g37 = g37 / 2;
+            g38 = g38 / 2;
+          }
+
+          return Math.round(g30 + g31 + g32 + g33 + g34 + g35 + g36 + g37 + g38);
+        };
+        // ref:7
+        $scope.ref7 = function (val) {
+          var num;
+          if (val === 'low') {
+            num = 0.1;
+          } else {
+            num = 0.05;
+          }
+          return num * $scope.ref6(val);
+        };
+        // ref:8
+        $scope.ref8 = function (val) {
+          return Math.round($scope.cal.param9 * $scope.ref7(val));
+        };
+        // ref:9
+        $scope.ref9 = function (val) {
+          return $scope.ref8(val) * 12;
+        };
+        // ref:10
+        $scope.ref10 = function (val) {
+          return 0.03 * $scope.cal.param1;
+        };
+        // ref:11
+        $scope.ref11 = function (val) {
+          var num;
+          if (val === 'low') {
+            num = 0.2;
+          } else {
+            num = 0.1;
+          }
+          return $scope.ref10(val) * num;
+        };
+        // ref:12
+        $scope.ref12 = function (val) {
+          return 450 * $scope.ref11(val);
+        };
+        // ref:13
+        $scope.ref13 = function (val) {
+          return Math.round($scope.ref9(val) + $scope.ref12(val) / 2);
+        };
+
+
+        $scope.var_revenue_integrity_proc_imp = Math.round($scope.ref13("low") / $scope.cal.adjustment);
+        $scope.var_revenue_integrity_proc_imp_high = Math.round($scope.ref13("high") / $scope.cal.adjustment);
 
         localStorageService.set('cal', $scope.cal);
 
---
+++ tests/revenue_integrity_process_improvement_high.js
@@ -0,0 +1,51 @@
+describe('calculator', function () {
+  var total_passngrs = element(by.model('cal.param1'));
+  var passngr_growth = element(by.model('cal.param2'));
+  var num_tickets_issued = element(by.model('cal.param3'));
+  var pc_tickets_reissued = element(by.model('cal.param4'));
+  var labour_costs = element(by.model('cal.param5'));
+  var air_revenue = element(by.model('cal.param6'));
+  var ann_cst_bse = element(by.model('cal.param7'));
+  var ann_dist_rate = element(by.model('cal.param8'));
+  var av_yield_p_ticket = element(by.model('cal.param9'));
+  var ann_fuel_rate = element(by.model('cal.param10'));
+  var cal_adjust = element(by.model('cal.adjustment'));
+  var var_revenue_integrity_proc_imp = element(by.binding('var_revenue_integrity_proc_imp_high'));
+  var ref1 = element(by.binding('ref1()'));
+  var ref2 = element(by.binding('ref2()'));
+  var ref3 = element(by.binding('ref3()'));
+  var ref4 = element(by.binding('ref4()'));
+  var ref5 = element(by.binding('ref5()'));
+  var ref6 = element(by.binding('ref6()'));
+  var ref7 = element(by.binding('ref7()'));
+  var ref8 = element(by.binding('ref8()'));
+  var ref9 = element(by.binding('ref9()'));
+  var ref10 = element(by.binding('ref10()'));
+  var ref11 = element(by.binding('ref11()'));
+  var ref12 = element(by.binding('ref12()'));
+  var ref13 = element(by.binding('ref13()'));
+
+  function addNumbers () {
+    total_passngrs.clear().sendKeys(6500000);
+    passngr_growth.clear().sendKeys(3);
+    num_tickets_issued.clear().sendKeys(3611111);
+    pc_tickets_reissued.clear().sendKeys(10);
+    labour_costs.clear().sendKeys(7);
+    air_revenue.clear().sendKeys(2500000000);
+    ann_cst_bse.clear().sendKeys(2565000000);
+    ann_dist_rate.clear().sendKeys(15);
+    av_yield_p_ticket.clear().sendKeys(100);
+    ann_fuel_rate.clear().sendKeys(34);
+    cal_adjust.clear().sendKeys(1000000);
+  }
+
+  beforeEach(function () {
+    browser.get('http://hoz_calculator.dev/#/calculator');
+  });
+
+  it('var_revenue_integrity_proc_imp_high should equal 37', function () {
+    //addNumbers();
+    expect(var_revenue_integrity_proc_imp_high.getText()).toEqual('13');
+  });
+
+});
